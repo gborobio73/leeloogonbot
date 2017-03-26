fe18a1204de6ab0ec6a06032ce5c66c702103bf4
Gonzalo Borobio <gborobio@gmail.com>
2017-03-26 16:52:42 +0300

    echo bot

--- src/main/java/com/leeloo/leeloogonbot/messageprocessors/EchoMessageProcessor.java
+++ src/main/java/com/leeloo/leeloogonbot/messageprocessors/EchoMessageProcessor.java
@@ -4,11 +4,14 @@ public class EchoMessageProcessor implements BotMessageProcessor {
 	
 	private String message;
 	public EchoMessageProcessor(String message) {
-		this.message = message;
+		this.message = message.toLowerCase();
 	}
 	@Override
 	public String get() {
-		return "no entiendo: " +message;
+		if ((message.contains("quien") || message.contains("quién"))&& message.contains("eres") ) {
+			return "I am Leeloo Bot";
+		}
+		return "no entiendo: " +message +". I can only aswer about weather e.g. 'weather Rovaniemi'";
 	}
 
 }
